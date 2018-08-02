fda6aad72a65190ac8b4eed1884e095ff9996dd6
Daniel Ciuraru <daniel.ciuraru@playbuzz.com>
2018-08-02 11:24:35 +0300

    adding pseudo selector using radium

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium from 'radium';
 
 class App extends Component {
   state = {
@@ -38,7 +39,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': { //pseudo selectors are added like this
+        backgroundColor: 'lightgreen',
+        color:'black'
+      }
     };
 
     let persons = null;
@@ -81,4 +86,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App); //higher order component - wrapping component adds functionality to this component
